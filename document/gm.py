d35e7d1736e5e800dd2e8569e1b1eff6e587b459
Dan F-M <danfm@nyu.edu>
2013-11-20 18:20:38 -0500

    adding list of parameters and updating PGM

--- document/gm.py
+++ document/gm.py
@@ -11,18 +11,23 @@ rc("text.latex",
 
 import daft
 
-pgm = daft.PGM([4.5, 3], origin=[-0.5, 1])
+pgm = daft.PGM([4.5, 2.6], origin=[-0.5, 1])
+
+pgm.add_plate(daft.Plate([0.4, 1.1, 3.1, 2.4], label=r"stars $n=1,\ldots,N$",
+                         position="bottom right"))
+pgm.add_plate(daft.Plate([0.5, 1.4, 2, 2], label=r"planets $k=1,\ldots,K$"))
 
 pgm.add_node(daft.Node("population", r"\population", 0.0, 3.0, fixed=True))
 
-pgm.add_node(daft.Node("planet", r"\planet", 1.0, 3.0))
-pgm.add_node(daft.Node("planetobs", r"\planetobs", 2.0, 3.0, observed=True))
-
-pgm.add_node(daft.Node("stellar", r"\stellar", 3.0, 3.0))
-pgm.add_node(daft.Node("stellarobs", r"$\stellarobs$", 3.0, 2.0,
+pgm.add_node(daft.Node("planet", r"$\planet_{kn}$", 1.0, 3.0))
+pgm.add_node(daft.Node("planetobs", r"$\planetobs_{kn}$", 2.0, 3.0,
                        observed=True))
 
-pgm.add_node(daft.Node("isobs", r"\isobs", 1.5, 2.0, observed=True))
+pgm.add_node(daft.Node("stellar", r"$\stellar_n$", 3.0, 3.0))
+pgm.add_node(daft.Node("stellarobs", r"$\stellarobs_n$", 3.0, 2.0,
+                       observed=True))
+
+pgm.add_node(daft.Node("isobs", r"$\isobs_{kn}$", 1.5, 2.0, observed=True))
 
 pgm.add_node(daft.Node("selection", r"\selection", 0.0, 2.0, fixed=True))
 
@@ -33,10 +38,6 @@ pgm.add_edge("stellar", "planetobs")
 pgm.add_edge("stellar", "isobs")
 pgm.add_edge("stellar", "stellarobs")
 pgm.add_edge("selection", "isobs")
-
-pgm.add_plate(daft.Plate([0.4, 1.4, 3.1, 2.2], label=r"stars",
-                         position="bottom right"))
-pgm.add_plate(daft.Plate([0.5, 1.5, 2, 2], label=r"planets"))
 
 pgm.render()
 pgm.figure.savefig("gm.png", dpi=150)
