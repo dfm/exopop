0b26ca8685c635324b14322b0456b89aa8eb55e4
Dan F-M <danfm@nyu.edu>
2014-03-26 13:29:21 -0400

    figures and results for first figure

--- document/code/load_data.py
+++ document/code/load_data.py
@@ -19,9 +19,10 @@ bp = os.path.join(base, "data")
 stars = pd.read_hdf(os.path.join(bp, "stlr.h5"), "stlr")
 G = 2945.4625385377644
 P = 10.0
-transit_lnprobs = np.log(stars.Rstar) + (np.log(4*np.pi*np.pi)
-                                         - np.log(G*P*P*stars.Mstar)) / 3
-transit_lnprob0 = np.median(transit_lnprobs)
+# transit_lnprobs = np.log(stars.Rstar) + (np.log(4*np.pi*np.pi)
+#                                          - np.log(G*P*P*stars.Mstar)) / 3
+# transit_lnprob0 = np.median(transit_lnprobs)
+transit_lnprob0 = -2.98353340397
 ln_period0 = np.log(P)
 
 
@@ -46,3 +47,19 @@ def load_candidates(censor, samples=0, rp_func=np.log):
     log_per_obs = np.array(log_per_obs, dtype=float)
     log_rp_obs = np.array(log_rp_obs, dtype=float)
     return population.Dataset(log_per_obs, log_rp_obs, censor)
+
+
+def load_petigura_bins(mylog=np.log):
+    ep_Rp_logbins = 0.5 * np.log10(2) * np.arange(9)
+    ep_Rp_lnbins = mylog(10 ** ep_Rp_logbins)
+    ep_Rp_values = np.array([12., 14.2, 18.6, 5.9, 1.9, 1.0, 0.9, 0.7])
+    norm = np.sum(ep_Rp_values * (ep_Rp_lnbins[1:] - ep_Rp_lnbins[:-1]))
+    ep_Rp_pdf = ep_Rp_values / norm
+
+    ep_p_logbins = np.log10([6.25, 12.5, 25, 50, 100])
+    ep_p_lnbins = mylog(10 ** ep_p_logbins)
+    ep_p_values = np.array([8.9, 13.7, 15.8, 15.2])
+    norm = np.sum(ep_p_values * (ep_p_lnbins[1:] - ep_p_lnbins[:-1]))
+    ep_p_pdf = ep_p_values / norm
+
+    return ep_p_lnbins, ep_Rp_lnbins, ep_p_pdf, ep_Rp_pdf
